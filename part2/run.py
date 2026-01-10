3cb5cee2d3c24d7b9bc8c1f03825e587a631c2c9
user76335 <11958@holbertonstudents.com>
2026-01-10 21:27:27 +0300

    Change app run configuration to specific host and port

--- part2/run.py
+++ part2/run.py
@@ -1,4 +1,7 @@
+# run.py
 from app import create_app
+
 app = create_app()
-if __name__ == '__main__':
-    app.run(debug=True)
+
+if __name__ == "__main__":
+    app.run(host="127.0.0.1", port=5001, debug=True)
