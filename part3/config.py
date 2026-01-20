9cb374e12fe5e7340b2889fa7fac1123a4b12d6d
faisal-Alazmi <fa41sa@hotmail.com>
2026-01-20 20:19:40 +0300

    Add configuration classes for different environments

---
+++ part3/config.py
@@ -0,0 +1,20 @@
+import os
+
+class Config:
+    """Base configuration"""
+    SECRET_KEY = os. getenv('SECRET_KEY', 'default-secret-key-change-in-production')
+    DEBUG = False
+
+class DevelopmentConfig(Config):
+    """Development configuration"""
+    DEBUG = True
+
+class ProductionConfig(Config):
+    """Production configuration"""
+    DEBUG = False
+    SECRET_KEY = os.getenv('SECRET_KEY')  # Must be set in production
+
+class TestingConfig(Config):
+    """Testing configuration"""
+    TESTING = True
+    DEBUG = True
